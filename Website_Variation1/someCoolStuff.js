343715849c43dbf40e4e40f7729d84d2c80fd0b6
xxlillusionlxx <jdale@umass.edu>
2014-03-26 04:33:54 -0400

    Personal Website
    Contents of my website

---
+++ Website_Variation1/someCoolStuff.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+$(function () {
+  
+/*
+$( document.body ).click(function() {
+  $( "div:hidden:first" ).fadeIn( "slow" );
+}); */
+
+	fade();
+
+});
+
+function fade() {
+
+  /*$( document.body ).click(function() {
+  	$( "div" ).fadeIn( "slow" );
+  }); */
+
+}

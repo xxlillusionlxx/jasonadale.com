2c6824e32bb567500a45e15c6d3e1f6a5b515fbf
xxlillusionlxx <jdale@student.umass.edu>
2014-06-26 01:04:24 -0400

    Removed
    Removed "addClass" call for the contacts section to prevent it from
    being too large

--- Website_Variation3/js/control.js
+++ Website_Variation3/js/control.js
@@ -48,7 +48,7 @@ $(function () {
 	//Fixes some weird issue with Chrome not showing BG image for projects section until all animations are done..
 	$('#jumbo').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
 		$('#projects').addClass('parallax-background');
-		$('#contact').addClass('parallax-background');
+		
 	});
 
 });

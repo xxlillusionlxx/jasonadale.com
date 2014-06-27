dac552b4932706a82184840cdba90f866db44e0a
xxlillusionlxx <jdale@student.umass.edu>
2014-06-26 20:04:02 -0400

    New Design
    New Design. New custom font being used.
    *background images changed to paper texture
    *text in about section changed
    *"Jason Dale" header text changed
    *animation modified
    *Overwrote some Bootstrap styling for the jumbotron

--- Website_Variation3/js/control.js
+++ Website_Variation3/js/control.js
@@ -11,24 +11,24 @@ $(function () {
 	$('#first').addClass('animated fadeInLeft');
 	
 	$('#first').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-		$('#one').css('border-bottom-style', 'solid');
-		$('#one').css('border-bottom-color', '#7a7a8a');
-		
+		$('#one').css('border-right-style', 'solid');
+		$('#one').css('border-right-color', '#7a7a8a');
+
 		$('#two').css('visibility', 'visible');
 		$('#second').addClass('animated fadeInUp');
 	});
 	
 	$('#second').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-		$('#two').css('border-bottom-style', 'solid');
-		$('#two').css('border-bottom-color', '#7a7a8a');
+		$('#two').css('border-right-style', 'solid');;
+		$('#two').css('border-right-color', '#7a7a8a');
 		
 		$('#three').css('visibility', 'visible');
 		$('#third').addClass('animated fadeInRight');
 	});
 	
 	$('#third').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-		$('#three').css('border-bottom-style', 'solid');
-		$('#three').css('border-bottom-color', '#7a7a8a');
+		$('#three').css('border-right-style', 'solid');
+		$('#three').css('border-right-color', '#7a7a8a');
 	});
 
 	$('#three').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {

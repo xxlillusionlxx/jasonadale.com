e1c4086d1195c4800b8815771959b783b03903f3
Jason Dale <jdale@umass.edu>
2015-03-18 03:23:40 -0400

    New Website Layout
    Mostly Testing out a new effect I tried to implement. Wanting no actual
    loading between view switching.

--- digitalocean_folder/www.jasonadale.com/public/javascripts/effects.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$(window).scroll(function(event) {
-		var y = $(this).scrollTop();
-		if(y >= 400) {
-			$('#jumboEdit').addClass('animated wiggle');
-			$('.codeSnippet').addClass('animated wiggle');
-		}
-	});
-});
